f8c2f084b97b044df55ce6ecdc1c608e8eb56023
pdk <peterkammermeierja@gmail.com>
2016-11-21 19:57:00 +0100

    Minor changes can be ignored

--- Game/src/Game.java
+++ Game/src/Game.java
@@ -5,6 +5,7 @@ public class Game {
     public static void main(String[] args) {
         Player player = new Player("Peter1", 5);
         Enemy enemy = new Enemy("Peter2", 1);
+        Enemy enemy2 = new Enemy("Hurensohn", 100);
         while (enemy.isAlice()) {
             player.attack(enemy);
         }
